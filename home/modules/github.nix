a4d186904c228a14e8b4dd2ed611623ab8c75618
YS-Raptor <yashan.sumanaratne@gmail.com>
2025-06-09 02:05:13 +1000

    removed some options from git

--- home/modules/github.nix
+++ home/modules/github.nix
@@ -3,9 +3,5 @@
     enable = true;
     userName = userSettings.github-username;
     userEmail = userSettings.email;
-
-    extraConfig.credential.helper = "manager";
-    extraConfig.credential."https://github.com".username = "YourUserName";
-    extraConfig.credential.credentialStore = "gpg";
   };
 }
