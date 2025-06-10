e2aa57af09918e7da5525e73f822dc18386d4a12
YS-Raptor <yashan.sumanaratne@gmail.com>
2025-06-10 16:59:35 +1000

    removed waybar restarting in posthook

--- sync-posthook.sh
+++ sync-posthook.sh
@@ -2,5 +2,4 @@
 
 # hyprland
 pgrep Hyprland &> /dev/null && echo "Reloading hyprland" && hyprctl reload &> /dev/null;
-pgrep .waybar-wrapped &> /dev/null && echo "Restarting waybar" && killall .waybar-wrapped && echo "Running waybar" && waybar &> /dev/null & disown;
 pgrep hyprpaper &> /dev/null && echo "Reapplying background via hyprpaper" && killall hyprpaper && echo "Running hyprpaper" && hyprpaper &> /dev/null & disown;
