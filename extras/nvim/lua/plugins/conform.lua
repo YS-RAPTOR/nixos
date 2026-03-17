0e7f785dd4fdbcee726b9a15ef2cb709661912d4
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2026-03-17 11:16:18 +1100

    the nix formatter uses configured indent rather than hardcoding 4

--- extras/nvim/lua/plugins/conform.lua
+++ extras/nvim/lua/plugins/conform.lua
@@ -9,7 +9,13 @@ return {
         },
         formatters = {
             nixfmt = {
-                append_args = { "--indent", "4" },
+                append_args = function(_, ctx)
+                    local indent = vim.bo[ctx.buf].shiftwidth
+                    if indent == 0 then
+                        indent = vim.bo[ctx.buf].tabstop
+                    end
+                    return { "--indent", tostring(indent) }
+                end,
             },
         },
     },
