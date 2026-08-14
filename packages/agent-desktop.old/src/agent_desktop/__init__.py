82e7bab96f0b768359609a9207d541991d83013b
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2026-08-14 21:56:26 +1000

    delete old agent-desktop

--- packages/agent-desktop.old/src/agent_desktop/__init__.py
+++
@@ -1,3 +0,0 @@
-"""On-demand graphical desktops for computer-use agents."""
-
-__version__ = "0.1.0"
--- packages/agent-desktop.old/src/agent_desktop/errors.py
+++
@@ -1,4 +0,0 @@
-class AgentDesktopError(Exception):
-    def __init__(self, message: str, code: str = "agent_desktop_error") -> None:
-        super().__init__(message)
-        self.code = code
